6033b023c1fba3c67fc9247df7c17929b241637b
Mahak Saxena <ms2722528@gmail.com>
2026-01-17 21:42:13 +0530

    update of new model validation

--- project/app/models.py
+++ project/app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import MaxLengthValidator,MinLengthValidator
+from django.core.exceptions import ValidationError
 # Create your models here.
 class Employee(models.Model):
     name = models.CharField(max_length=100,default='N/A')
@@ -20,6 +21,13 @@ class Employee(models.Model):
     #     # return f"{self.name} - {self.email}"
     #     # return f"{self.name}"
 
+def Emp1_contact(contact):
+    if not len(str(contact))==10:
+        raise ValidationError("Contact must be in 10 digits only")
+    if not contact.isdigit():
+        raise ValidationError("Contact must be only in number")
+
+        
 
 class Employee1(models.Model):
     name = models.CharField(max_length=100,validators=[MaxLengthValidator(10),MinLengthValidator(3)],   error_messages={
@@ -28,15 +36,24 @@ class Employee1(models.Model):
         'max_length': 'Name must not exceed 10 characters',
     })
     email = models.EmailField(unique=True)
-    contact = models.CharField(max_length=15)
+    contact = models.CharField(max_length=15,validators=[Emp1_contact])
     image = models.ImageField(upload_to='employee_profiles/', null=True, blank=True)
     password = models.CharField(max_length=128,default='N/A')
     department=models.CharField(max_length=50,null=True)
     d_code = models.CharField(max_length=20,null=True)
     d_des = models.CharField(max_length=50,null=True)
 
+    def clean(self):
+        if not len(str(self.password))==4:
+            raise ValidationError("Password must be 4 length")
+
+    def save(self,*args,**kwargs):
+        self.full_clean()
+        super().save(*args,**kwargs)
+
     def __str__(self):
         return self.name
+    
 
 
 class Department(models.Model):
